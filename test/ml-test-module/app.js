689ff0e37a689de1302ea7872335b5c49b8ceffb
Matt Copperwaite <mattcopp+git@gmail.com>
2014-02-06 14:08:39 +0000

    hardened the tests for module_loader

--- test/ml-test-module/app.js
+++ test/ml-test-module/app.js
@@ -1,5 +1,6 @@
-module.exports = function (options) {
-    return {
-        "options": options
+module.exports = {
+    "name": "ml-test-module",
+    "run": function(options) {
+        return options
     }
 }
--- test/module_loader.js
+++ test/module_loader.js
@@ -1,8 +1,37 @@
 var ModuleLoader = require("../module_loader"),
     assert = require("assert");
+    
 
-var module_loader_test_1 = new ModuleLoader("test");
-
-module_loader_test_1.load_module("ml-test-module");
-
-assert.ok(module_loader_test_1);
+describe('Module Loader', function(){
+    describe('initialise', function(){
+        it("should load successfully", function () {
+            var module_loader_test = new ModuleLoader("test");
+        });
+    });
+    
+    describe('initialise multiple', function(){
+        var module_loader_test_1 = new ModuleLoader("test");
+        var module_loader_test_2 = new ModuleLoader("test");
+        
+        module_loader_test_1.modules.push("test");
+        it("should not be the same object", function () {
+            assert.notEqual(module_loader_test_1.modules, module_loader_test_2.modules);
+        });
+    });
+    
+    describe('loads module', function(){
+        var test_module_name = "ml-test-module";
+        var module_loader_test = new ModuleLoader("test");
+        
+        module_loader_test.load_module(test_module_name);
+        it("should populate the module", function () {
+            assert.ok(module_loader_test.modules);
+        });
+        it("should only have one module", function () {
+            assert.equal(module_loader_test.modules.length, 1);
+        });
+        it("should contain the test module", function () {
+            assert.equal(module_loader_test.modules[0].name, test_module_name);
+        });
+    });
+});
